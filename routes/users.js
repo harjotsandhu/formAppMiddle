912406eacd6bca112b7aa400cc1447d959a07edc
Harjot Sandhu <hsandhu4@masonlive.gmu.edu>
2017-04-10 17:28:47 -0400

    added column id as primarykey in db, phone,email unique constraint

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,6 @@ router.get('/', function(req, res, next) {
     else {
       let data=[];
          result.rows.forEach( function(row) {
-           row.id = row.phone;
            data.push(row);
          });
          res.status(200).send({users : data});
@@ -29,11 +28,58 @@ router.get('/', function(req, res, next) {
     });
   });
 
+  router.get('/:id', function(req, res, next) {
+    let userId = req.params.id;
+    let getUser = "select * from users where id=" + userId;
+    clientConnection.query(getUser, function(err, result) {
+      if(err) {
+      res.status(500).send({
+          "status":"500","code":"90402","userMessage":"Error","developerMessage":err
+        });
+      }
+      else {
+        let data=[];
+           result.rows.forEach( function(row) {
+             data.push(row);
+           });
+           res.status(200).send({users : data});
+         }
+      });
+    });
+
+  router.put('/:id', function (req, res) {
+    let update = req.body.user;
+    let userId = req.params.id;
+    let returnQuery= "select * from users where id=" + userId;
+    let query=  clientConnection.query('Update users SET firstname=($1), lastname=($2), phone=($3),status=($4),email=($5) where id='+ userId,
+     [update.firstname, update.lastname, update.phone, update.status,update.email], function(err, result) {
+       if(err) {
+       res.status(500).send({
+           "status":"500","code":"90402","userMessage":"Error","developerMessage":err
+         });
+       }
+       else {
+         clientConnection.query(returnQuery , function (err, result) {
+           if(err) {
+           res.status(500).send({
+               "status":"500","code":"90402","userMessage":"Error","developerMessage":err
+             });
+           }
+           else {
+            res.status(200).send({orders : result.rows});
+          }
+         });
+       }
+    });
+  });
+
   router.post('/', function(req, res, next) {
     let user= req.body.user;
+    let userId='nextval('+ "\'id\'"+ ')';
+    let curr_id='currval('+ "\'id\'" + ')';
     console.log(user);
-    let createUser = 'insert into users values('  +user.phone + ',' + '\'' + user.firstname+ '\'' + ','+ '\'' + user.lastname+ '\'' + ',' + '\''+ user.email+ '\'' + ',' + user.status + ')';
-    let fetchUser = 'select * from users where phone=' + user.phone;
+    let createUser = 'insert into users values('  +user.phone + ',' + '\'' + user.firstname+ '\'' + ','+ '\'' + user.lastname+ '\'' + ',' + '\''+ user.email+ '\'' + ',' + user.status+ ','  + userId + ')';
+    let fetchUser = 'select * from users where id=' + curr_id;
     clientConnection.query(createUser, function(err, result) {
       console.log(createUser);
       if(err) {
@@ -51,8 +97,6 @@ router.get('/', function(req, res, next) {
           else{
           let data=[];
              result.rows.forEach( function(row) {
-               row.id=1;
-               console.log(row);
                data.push(row);
              });
              res.status(200).send({users : data});
@@ -63,7 +107,7 @@ router.get('/', function(req, res, next) {
     });
   router.delete('/:id', function(req, res, next) {
     let userId= req.params.id;
-    let deleteRequest = "delete from users where phone="+ userId;
+    let deleteRequest = "delete from users where id="+ userId;
     console.log(deleteRequest);
     clientConnection.query(deleteRequest, function(err, result) {
       if(err) {
